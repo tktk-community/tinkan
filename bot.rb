06541a92f90141e994005de61ae5e838ae79f11a
David Celis <me@davidcel.is>
2024-12-16 21:42:22 -0800

    @mention moderators when forwarding reports

--- bot.rb
+++ bot.rb
@@ -5,9 +5,12 @@ require "discordrb"
 DISCORD_BOT_TOKEN = ENV["DISCORD_BOT_TOKEN"]
 DISCORD_SERVER_ID = ENV["DISCORD_SERVER_ID"].to_i
 MODERATORS_CHANNEL_ID = ENV["MODERATORS_CHANNEL_ID"].to_i
+MODERATOR_ROLE_ID = ENV["MODERATORS_ROLE_ID"].to_i
 PRIVATE_CATEGORY_ID = ENV["PRIVATE_CATEGORY_ID"].to_i
 
 bot = Discordrb::Bot.new(token: DISCORD_BOT_TOKEN, intents: [:server_messages], ignore_bots: true)
+
+Kernel.at_exit { bot.stop }
 
 # /mods <message>
 #
@@ -19,7 +22,7 @@ end
 bot.application_command(:mods) do |event|
   message = event.options["message"]
 
-  event.bot.send_message(MODERATORS_CHANNEL_ID, "Message from #{event.user.mention} in #{event.channel.mention}:\n\n#{message}")
+  event.bot.send_message(MODERATORS_CHANNEL_ID, "<@&#{MODERATOR_ROLE_ID}> Message from #{event.user.mention} in #{event.channel.mention}:\n\n#{message}")
 
   event.respond(content: "I've forwarded your message to the moderators and they'll respond as soon as possible!", ephemeral: true)
 end
@@ -104,7 +107,5 @@ bot.application_command(:invite) do |event|
 
   event.respond(content: "Okay, I've added #{user.mention} to this channel!", ephemeral: true)
 end
-
-Kernel.at_exit { bot.stop }
 
 bot.run
